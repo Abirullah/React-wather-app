a059ab426f68453831bf29c74166761d66971e27
Abir Afridi <abirafridi87@gmail.com>
2025-02-11 17:34:24 +0500

    wather app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Watherapp from './Watherapp'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Watherapp></Watherapp>
+    </>
+  )
+}
+
+export default App
